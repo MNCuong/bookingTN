952a44e191c23382f6c8cfd0089aee41c59c6c8e
MNCuong <cuongll912003@gmail.com>
2025-02-25 15:18:07 +0700

    feature: payment by vnpay

--- src/main/java/com/example/booking/Config/VnPayConfig.java
+++ src/main/java/com/example/booking/Config/VnPayConfig.java
@@ -1,15 +1,10 @@
 package com.example.booking.Config;
 
-import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -29,71 +24,11 @@ public class VnPayConfig {
     @Value("${vnpay.TmnCode}")
     public String vnp_TmnCode;
 //    @Value("${vnpay.secretKey}")
-    public static String secretKey="70FO4O5J6DYK8N8RHJ2NPFDNBJ3WKPUP";
+    public static String secretKey="8SSKHMNVIVYJ2VUVR7TM42ILG1F44F3R";
     @Value("${vnpay.apiUrl}")
     public String vnp_ApiUrl;
-    public String md5(String message) {
-        String digest = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] hash = md.digest(message.getBytes("UTF-8"));
-            StringBuilder sb = new StringBuilder(2 * hash.length);
-            for (byte b : hash) {
-                sb.append(String.format("%02x", b & 0xff));
-            }
-            digest = sb.toString();
-        } catch (UnsupportedEncodingException ex) {
-            digest = "";
-        } catch (NoSuchAlgorithmException ex) {
-            digest = "";
-        }
-        return digest;
-    }
-
-    public static String Sha256(String message) {
-        String digest = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(message.getBytes("UTF-8"));
-            StringBuilder sb = new StringBuilder(2 * hash.length);
-            for (byte b : hash) {
-                sb.append(String.format("%02x", b & 0xff));
-            }
-            digest = sb.toString();
-        } catch (UnsupportedEncodingException ex) {
-            digest = "";
-        } catch (NoSuchAlgorithmException ex) {
-            digest = "";
-        }
-        return digest;
-    }
-
-    //Util for VNPAY
-    public static String hashAllFields(Map fields) {
-        List fieldNames = new ArrayList(fields.keySet());
-        Collections.sort(fieldNames);
-        StringBuilder sb = new StringBuilder();
-        Iterator itr = fieldNames.iterator();
-        while (itr.hasNext()) {
-            String fieldName = (String) itr.next();
-            String fieldValue = (String) fields.get(fieldName);
-            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-                sb.append(fieldName);
-                sb.append("=");
-                sb.append(fieldValue);
-            }
-            if (itr.hasNext()) {
-                sb.append("&");
-            }
-        }
-        return hmacSHA512(secretKey,sb.toString());
-    }
-
     public static String hmacSHA512(final String key, final String data) {
-        log.info("vnPayConfig.secretKey=============" +secretKey);
-
         try {
-
             if (key == null || data == null) {
                 throw new NullPointerException();
             }
@@ -135,5 +70,17 @@ public class VnPayConfig {
             sb.append(chars.charAt(rnd.nextInt(chars.length())));
         }
         return sb.toString();
+    }
+    public static String getPaymentURL(Map<String, String> paramsMap, boolean encodeKey) {
+        return paramsMap.entrySet().stream()
+                .filter(entry -> entry.getValue() != null && !entry.getValue().isEmpty())
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry ->
+                        (encodeKey ? URLEncoder.encode(entry.getKey(),
+                                StandardCharsets.US_ASCII)
+                                : entry.getKey()) + "=" +
+                                URLEncoder.encode(entry.getValue()
+                                        , StandardCharsets.US_ASCII))
+                .collect(Collectors.joining("&"));
     }
 }
--- src/main/java/com/example/booking/Service/PaymentService.java
+++ src/main/java/com/example/booking/Service/PaymentService.java
@@ -6,6 +6,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.http.HttpRequest;
 
 public interface PaymentService {
-    String getPay(long amount, String bankCode) throws UnsupportedEncodingException;
+    String getPay(long amount, String bankCode, HttpServletRequest request) throws UnsupportedEncodingException;
     int orderReturn(HttpServletRequest request);
 }
